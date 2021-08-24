51495eed3eef02e5f571a027f9df43801c608b97
damarvijis <damar.j@mail.ugm.ac.id>
2021-08-24 22:17:30 +0700

    asd

---
+++ index.js
@@ -0,0 +1,32 @@
+let line = document.getElementById("slogan");
+let txts = [" UI/UX Designer", " Full-Stack Developer", " Freelancer"];
+let speed = 100;
+async function typewriter(txt) {
+  for (let i = 0; i < txt.length; i++) {
+    line.innerHTML += txt[i];
+    await delay(speed);
+  }
+}
+async function reverseTypewriter(txt) {
+  for (let i = txt.length; i > 0; i--) {
+    line.innerHTML = line.innerHTML.slice(0, -1);
+    await delay(speed);
+  }
+}
+async function writeLoop() {
+  for (let i = 0; i < txts.length; i++) {
+    await typewriter(txts[i]);
+    await delay(1000);
+    await reverseTypewriter(txts[i]);
+    await delay(10);
+  }
+  writeLoop();
+}
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+writeLoop();
